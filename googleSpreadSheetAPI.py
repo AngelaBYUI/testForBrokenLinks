b9fdf5affc9e9192f40a9f0bee8335279206ec73
AngelaBYUI <che25017@byui.edu>
2025-10-09 11:40:57 -0600

    cleared some useless comments

--- googleSpreadSheetAPI.py
+++ googleSpreadSheetAPI.py
@@ -45,17 +45,6 @@ try: #get the file and the specific sheet
                         print("broken")
 
 
-        # for row in range(1, num_rows+1):
-        #
-        #     #check if there's a link in the cell of this line
-        #     if not col_data[row-1]:
-        #         cell_location = rowcol_to_a1(row, 7) #rowcol_to_a1 is a fuction from gspread library; (row, col) e.g. (4,3) means cell C4.
-        #         cell_to_mark_empty.append({
-        #             'range': cell_location,
-        #             'values': [["empty"]] #means the cell with url doesn't have data. e.g. it's the name of the section in this line.
-        #         })
-        #
-        #
         if cell_to_mark_empty:
             worksheet.batch_update([{
                 "range":cell["range"], # the range in cell["range"] is the same as line 25, and it's a key word for line 25 so we can't rename it.
@@ -68,36 +57,11 @@ try: #get the file and the specific sheet
             ])
 
 
-        #
-        # # check the url
-        # for idx, cell in enumerate(col_data, start=1): #start=1 bc it needs to match the row number, if we don't put start=1, it'll start from 0 but there's no row 0 in spreadsheet
-        #     the_link = re.search(r'https?:\/\/.*', cell)
-        #     if the_link:
-        #         print(idx)
-        #         check_links_func(the_link.group())
-        #     else:
-        #         print(idx,cell)
 
-        #line for row in range(1, num_rows+1): to print(idx,cell)
-
-
-
-        #
-        # try: #get and mark the non-url line in the WhatsApp links sheet
-        #     for col in col_data:
-        #         if col == "":
-        #             rowNumber = 1
-        #             print(col)
-        #             print("It's empty")
-        # except gspread.exceptions.APIError as e:
-        #     print("Error happened for marking the empty data")
 
     except gspread.exceptions.APIError as e:
         print(f"An API error occurred: {e}")
 
-    # Get all values from the worksheet and print them.
-    # data = worksheet.get_all_values()
-    # print(data)
 
 except gspread.exceptions.SpreadsheetNotFound:
     print("Error: Spreadsheet not found. Check the ID and sharing permissions.")
