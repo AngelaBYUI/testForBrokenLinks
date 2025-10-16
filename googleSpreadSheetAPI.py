4e0db22d58bb5dbf689c7954a35b0f7f968de89e
AngelaBYUI <che25017@byui.edu>
2025-10-16 08:40:06 -0600

    1. add time.sleep(10) after checking every ten links. 2. add two lines for checking from a specific data.

--- googleSpreadSheetAPI.py
+++ googleSpreadSheetAPI.py
@@ -3,6 +3,7 @@ from gspread import cell
 from gspread.utils import rowcol_to_a1
 from testForBrokenLinks import check_links_func
 import re
+import time
 
 # Use the credentials file to authenticate the service account.
 gc = gspread.service_account(filename='credentials.json')
@@ -10,18 +11,21 @@ gc = gspread.service_account(filename='credentials.json')
 # Open a Google Sheet by its ID.
 # Replace 'YOUR_SPREADSHEET_ID' with the ID from your sheet's URL.
 try: #get the file and the specific sheet
-    spreadsheet = gc.open_by_key('14Ipv7V2Is7FbfDvDAWMZ6aCQp6cWs8iPGIpPHfpIUfU') #the key is url for the sheet but only the strings between /d/ and /edit
+    spreadsheet = gc.open_by_key('14E2jkyH9Oo26kDikeIip9FWQzEarPTqSkutRxK9Kk-g') #the key is url for the sheet but only the strings between /d/ and /edit
     worksheet = spreadsheet.get_worksheet(0) #to access the sheet by index. The first sheet should be 0 and so on.
     try: #get the dta from specific column but restrict in the len(rows) bc we don't want to mark a thousand empty lines.
         allDataInList = worksheet.get_all_values()
         num_rows = len(allDataInList) #to get how many lines/rows have data and we will restrict the marking stuff in this range.
-        col_data = worksheet.col_values(2)  # column 'A' is 1, 'B' is 2, and so on.
+        col_data = worksheet.col_values(6)  # column 'A' is 1, 'B' is 2, and so on.
         col_data +=[""]*(num_rows-len(col_data)) #Pad the list if it's shorter than num_rows
 
         cell_to_mark_empty=[] #to catch the cells which need to show there's no url
         cell_to_mark_broken=[]
         i=0
-        for row, cell in enumerate(col_data, start=1):
+        link_check_count = 0 #for the sleep(); gonna make it sleep every ten links
+        # for row, cell in enumerate(col_data, start=1): #This line for all data
+        for row in range(1172, num_rows + 1): #This line and next line are for starting from specific data
+            cell = col_data[row - 1] #for staring from specific data
             if not cell.strip():  # if the cell is empty or just spaces
                 cell_location = rowcol_to_a1(row, 7)
                 cell_to_mark_empty.append({
@@ -36,6 +40,7 @@ try: #get the file and the specific sheet
                     i=i+1
                     print(i)
                     is_broken=check_links_func(the_link.group())#this function will return true or false
+                    link_check_count += 1
                     if is_broken:
                         cell_location = rowcol_to_a1(row, 7)
                         cell_to_mark_broken.append({
@@ -43,6 +48,9 @@ try: #get the file and the specific sheet
                             'values': [["broken"]]
                         })
                         print("broken")
+                if link_check_count % 10 == 0:
+                    print("Sleeping for 10 seconds...")
+                    time.sleep(10)
 
 
         if cell_to_mark_empty:
